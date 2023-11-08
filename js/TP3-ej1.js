ffde47946d9f5a314bf2daec3316892c560369c3
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-08 11:53:41 -0300

    ejercicio1 completo

---
+++ js/TP3-ej1.js
@@ -0,0 +1,22 @@
+const meses= [
+    'Enero',
+    'Febrero',
+    'Marzo',
+    'Abril',
+    'Mayo',
+    'Junio',
+    'Julio',
+    'Agosto',
+    'Septiembre',
+    'Octubre',
+    'Noviembre',
+    'Diciembre'
+
+];
+
+document.write(`<h1>Lista de Meses</h1>`);
+document.write(`<ul>`);
+for (let i=0; i < meses.length; i++){
+    document.write(`<li>${meses[i]}</li>`);
+}
+document.write(`</ul>`);
