d9936205f53c0f8bc5fa8a2114b4c3bfbad291c8
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-08 19:51:03 -0300

    ejercicio2 completo

--- js/TP3-ej2.js
+++ js/TP3-ej2.js
@@ -10,6 +10,7 @@ do{
 document.write(`<p>EL arreglo de ciudades tiene ${ciudades.length} elementos</p>`);
 
 document.write(`<ul>`)
+
 for( let i=0; i<(ciudades.length); i++){
 
 document.write(`<li>${ciudades[i]}</li>`)
@@ -23,4 +24,17 @@ document.write(`<p>Elemento 1er posicion:${ciudades[1]}</p>`);
 document.write(`<p>Elemento 3er posicion:${ciudades[3]}</p>`);
 document.write(`<p>Elemento ultima posicion:${ciudades[ciudades.length-1]}</p>`);
 
+
+
 document.write(`</ul>`);
+
+ciudades.splice(ciudades.length,0,'Paris')
+document.write(`<ul>`)
+
+for( let i=0; i<(ciudades.length); i++){
+
+document.write(`<li>${ciudades[i]}</li>`)
+}
+document.write(`</ul>`);
+
+document.write(`<ul>`);
