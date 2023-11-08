a39860d0e1be0ee869ad798ad093bdb8f0e6c3a3
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-08 18:43:41 -0300

    ejercicio2 hasta la segunda pelotita

--- js/TP3-ej2.js
+++ js/TP3-ej2.js
@@ -9,11 +9,14 @@ do{
 } while(confirm('desea ingresar otra ciudad?'));
 document.write(`<p>EL arreglo de ciudades tiene ${ciudades.length} elementos</p>`);
 
-document.write(`<ul>`)
+//for( let i=0; i<=(ciudades.length); i++){
+//console.log(i)}
+
+document.write(`<ul>`);
    
 
-document.write(`<p>Elemento 1er posicion:${ciudades[1]}</p>`)
-document.write(`<p>Elemento 3er posicion:${ciudades[3]}</p>`)
-document.write(`<p>Elemento ultima posicion:${ciudades[`${ciudades.length}`]}</p>`)
+document.write(`<p>Elemento 1er posicion:${ciudades[1]}</p>`);
+document.write(`<p>Elemento 3er posicion:${ciudades[3]}</p>`);
+document.write(`<p>Elemento ultima posicion:${ciudades[ciudades.length-1]}</p>`);
 
-document.write(`</ul>`)
+document.write(`</ul>`);
