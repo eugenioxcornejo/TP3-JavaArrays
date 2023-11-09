9d1dd944adf6ee286c323e7e29ee0c7add085075
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-09 11:10:29 -0300

    se agrego fromato centrado y detalles

--- js/TP3-ej3.js
+++ js/TP3-ej3.js
@@ -6,14 +6,15 @@ document.write(`<table> <tbody>`);
 
 
 for(i=0;i<50;i++){  
-  document.write(`<tr>`)
-    let resultados1=Math.floor(Math.random()*6)
+
+  document.write(`<tr>`);
+    let resultados1=Math.floor(Math.random()*6);
 console.log(`resulatado primer dado ${resultados1}`);
     let resultados2=Math.floor(Math.random()*6)
 console.log(`resultado segundo dado ${resultados2}`);
 let suma=(resultados1+resultados2);
 //document.write(`<td>${suma}</td>`);
-dados.push(suma)
+dados.push(suma);
 
 }
 
@@ -24,22 +25,18 @@ document.write(`<td><h2>suma dados</h2></td> <td><h2>Apariciones</h2></td>  `);
 const repetido=[];
 for (x=0; x<50;x++){
   
-  document.write(`<tr>`)
+  document.write(`<tr>`);
 const repetido1= dados.filter((numero) => numero===(dados[x]));
 console.log(`se repite ${repetido1}`);
 
-document.write(`<td> ${dados[x]}</td>  <td>${repetido1.length}</td>`)
+document.write(`<td> ${dados[x]}</td>  <td>${repetido1.length}</td>`);
 repetido.push(repetido1);
 
 }
-document.write(`</tr>`)
-document.write(`</tr>`)
+document.write(`</tr>`);
+document.write(`</tr>`);
 
 
 
-document.write(`</table></tbody>`)
+document.write(`</table></tbody>`);
 
-//array.forEach((1) => { 
-  //  repetido[1] = (repetido[1] || 0)+1;
-    
-//});
