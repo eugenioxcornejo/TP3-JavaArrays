d8c8699ac4553576cd5b0a9691fd3110f0f7a6bf
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-09 12:09:09 -0300

    ejercicio4 completo

---
+++ js/TP3-ej4.js
@@ -0,0 +1,14 @@
+function paroimpar(numeroingresado){
+const numeroporconsola = numeroingresado;
+console.log(numeroporconsola)
+
+if(0 === numeroporconsola%2){
+    document.write('El numero es par');
+    
+} else if (0 !== numeroporconsola%2) 
+   document.write('El numero ingresado es impar');
+
+}
+
+paroimpar(2)
+
