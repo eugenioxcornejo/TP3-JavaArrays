55ca19dc9b8762d19cbcc48e31188cc76188327f
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-09 07:56:41 -0300

    ejercicio3 completo falta estetica y repeticion todos los nuemros

--- js/TP3-ej3.js
+++ js/TP3-ej3.js
@@ -1,4 +1,4 @@
-const Dado1 =[];
+const dados =[];
 
 for(i=0;i<50;i++){
     let resultados1=Math.floor(Math.random()*6)
@@ -7,4 +7,15 @@ console.log(`resulatado primer dado ${resultados1}`);
 console.log(`resultado segundo dado ${resultados2}`);
 let suma=(resultados1+resultados2);
 document.write(`<p>${suma}</p>`);
-Dado1.push(suma)}
+dados.push(suma)}
+
+
+
+const repetido1= dados.filter((numero) => numero===(dados[0]));
+console.log(`se repite ${repetido1}`);
+document.write(`se repite ${repetido1.length} veces`)
+
+//array.forEach((1) => { 
+  //  repetido[1] = (repetido[1] || 0)+1;
+    
+//});
